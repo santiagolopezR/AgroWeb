8375186ec6261979b25908e1f169e059b63b53d5
santiagolopezR <198632381+santiagolopezR@users.noreply.github.com>
2026-07-16 18:18:23 -0500

    Update settings.py

--- cultivos/settings.py
+++ cultivos/settings.py
@@ -77,13 +77,29 @@ WSGI_APPLICATION = 'cultivos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import os
+from urllib.parse import urlparse
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    db = urlparse(DATABASE_URL)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': db.path[1:],
+            'USER': db.username,
+            'PASSWORD': db.password,
+            'HOST': db.hostname,
+            'PORT': db.port,
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
