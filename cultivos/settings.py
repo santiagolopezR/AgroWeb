25c8977139e372f698af90165b55fef930cca39f
santiagolopezR <198632381+santiagolopezR@users.noreply.github.com>
2026-07-23 16:39:55 -0500

    seguridad

--- cultivos/settings.py
+++ cultivos/settings.py
@@ -10,10 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kurpds*8st*9gfu@=_9y=vq1tt+-m_zmxhn6^ew844cg*v+cyi'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     'agroweb-vv4b.onrender.com',
