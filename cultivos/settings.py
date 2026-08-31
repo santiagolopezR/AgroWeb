56bf5d3c3e0a87e4915a18ebf9d474ce427018c5
santiagolopezR <198632381+santiagolopezR@users.noreply.github.com>
2026-08-31 08:56:10 -0500

    seguridad

--- cultivos/settings.py
+++ cultivos/settings.py
@@ -3,6 +3,11 @@
 # ==========================================
 # REST FRAMEWORK CONFIGURATION
 # ==========================================
+from pathlib import Path
+from decouple import config
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -98,11 +103,7 @@ LOGGING = {
         },
     },
 }
-from pathlib import Path
-from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
